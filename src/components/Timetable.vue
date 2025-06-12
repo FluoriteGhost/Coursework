24b148d65eb763a8a442590098634f996c0b7504
FluoriteGhost <chunina.nika@gmail.com>
2025-06-12 22:42:29 +0700

    Add files via upload

--- src/components/Timetable.vue
+++ src/components/Timetable.vue
@@ -19,17 +19,18 @@ const store = useCounterStore()
 </ul>
 
 <form>
+  <p>Хотите записаться на занятия?</p>
   <div class="mb-3">
-    <label for="userid" class="form-label">User Id</label>
+    <label for="userid" class="form-label">Ваш номер телефона</label>
     <input type="userid" v-model.number="store.userId" class="form-control" id="userid">
   </div>
     <div class="mb-3">
-    <label for="username" class="form-label">Username</label>
+    <label for="username" class="form-label">Имя</label>
     <input type="username" v-model="store.title" class="form-control" id="username">
   </div>
 <div class="mb-3">
-  <label for="post" class="form-label">Post body</label>
-  <textarea class="form-control" v-model="store.body" id="post" rows="3"></textarea>
+  <label for="post" class="form-label">На какие занятия вы записываетесь?</label>
+  <textarea class="form-control" v-model="store.body" id="post"></textarea>
 </div>
   <button type="submit" class="btn btn-primary" @:click="store.sendrequest()">Submit</button>
 </form>
