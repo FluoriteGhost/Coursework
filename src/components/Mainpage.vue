9cd9a31fe87a5463d0aff7f529ffcfd3586de0b2
FluoriteGhost <chunina.nika@gmail.com>
2025-06-13 09:16:57 +0700

    Add files via upload

--- src/components/Mainpage.vue
+++ src/components/Mainpage.vue
@@ -11,6 +11,6 @@ store.informdata()
     </div>
     <img scr="./components/images/hedgehog.jpg" alt="hedgehog">
     <p>Группа ВК</p>
-    <a href="https://vk.com/club36624001"><img scr="./VK.png" width="100" height="100" alt="https://vk.com/club36624001"></a>
+    <a href="https://vk.com/club36624001"><img scr="https://vk.com/photo210582856_457241858" width="100" height="100" alt="https://vk.com/club36624001"></a>
 </main>
 </template>
