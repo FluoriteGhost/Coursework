c161ec5586ec1870da274b4c786fe3007e23a7fc
FluoriteGhost <chunina.nika@gmail.com>
2025-06-13 10:38:06 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,10 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div>
     <Myheader/>
-    </div>
-  </header>
   <main>
     <RouterView />
   </main>
-  <footer>
     <Myfooter/>
-  </footer>
 </template>
 
-<style scoped>
-header
-{
-  display: flex;
-  position:absolute;
-  top: 0px;
-  background-color: #14471E;
-}
-footer
-{
-  display: flex;
-  position: fixed;
-  bottom: 0px; 
-  color:black;
-  background-color: #14471E;
-}
-</style>
